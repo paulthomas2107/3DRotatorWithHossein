0caff49d15c2b59df582833eeae2356fdb94f680
Paul Thomas <paulthomas2107@gmail.com>
2023-03-10 17:15:24 +0000

    More.....

---
+++ app.js
@@ -0,0 +1,6 @@
+const sliderContainer = document.querySelector('.slider-container');
+const sliderContent = sliderContainer.querySelector('.slider-content');
+const sliderItems = sliderContent.querySelector('.slider-items');
+const sliderItem = sliderItems.querySelector('.slider-item');
+
+
